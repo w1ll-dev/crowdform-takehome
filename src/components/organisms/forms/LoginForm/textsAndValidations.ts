a825865799d5d2919b07dfa1afaecef0a7d9bafa
Will Gomes <w1llmarcio.dev@hotmail.com>
2024-01-19 08:59:22 -0300

    feat(login-screen): add texts and validations for login form
    on this file we have all texts that will be used for form and the validations that will be applied,
    with they own error. This validations are implemented with yup (https://github.com/jquense/yup) and
    object schema validation generally used with react hook form

---
+++ src/components/organisms/forms/LoginForm/textsAndValidations.ts
@@ -0,0 +1,44 @@
+import * as yup from "yup";
+import { MIN_PASSWORD_CHAR } from "../constants";
+
+const texts = {
+  login: "Login",
+  invalidCredentials: "😔 Invalid credentials",
+  pleaseTryAgain: "Please try again.",
+  forgotYourPassword: "Forgot your password ?",
+  dontHaveAnAccount: "Don't have an account?",
+  signUp: "Sign Up",
+};
+
+const textValidations: Record<
+  "errors" | "labels" | "placeholders",
+  Record<keyof LoginFieldValues, string>
+> = {
+  errors: {
+    email: "Please provide a valid email address.",
+    password: `Please provide a password of at least ${MIN_PASSWORD_CHAR} characters.`,
+  },
+  labels: {
+    email: "Email",
+    password: "Password",
+  },
+  placeholders: {
+    email: "Email",
+    password: "Password",
+  },
+};
+
+const loginValidations: YupSchemaShape<LoginFieldValues> = {
+  email: yup
+    .string()
+    .email(textValidations.errors.email)
+    .required(textValidations.errors.email),
+  password: yup
+    .string()
+    .min(MIN_PASSWORD_CHAR, textValidations.errors.password)
+    .required(textValidations.errors.password),
+};
+
+const yupLoginSchema = yup.object(loginValidations);
+
+export { textValidations, texts, yupLoginSchema, loginValidations };

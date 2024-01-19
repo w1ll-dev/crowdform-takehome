f7f2749b2a5984721564309614d3ec5679f98708
Will Gomes <w1llmarcio.dev@hotmail.com>
2024-01-19 08:22:46 -0300

    feat(login-screen): add opened eye icon

--- src/assets/iconDimension.ts
+++ src/assets/iconDimension.ts
@@ -7,6 +7,10 @@ const IconDimensions: Record<IconName, MediaDimensions> = {
     width: 50,
     height: 73,
   },
+  "opened-eye": {
+    width: 18,
+    height: 13,
+  },
 };
 
 export { IconDimensions };
--- src/assets/iconName.ts
+++ src/assets/iconName.ts
@@ -1,5 +1,6 @@
 const Icons = {
   "lock-circle": require("./images/files/lock-circle.png"),
+  "opened-eye": require("./images/files/opened-eye.png"),
 };
 
 type IconName = keyof typeof Icons;
